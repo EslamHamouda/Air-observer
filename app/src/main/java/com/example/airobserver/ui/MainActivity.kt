43354277c834d97e8e68ff86598900fa8023c311
PC-D-022 <Eslamsaber1>
2022-12-11 02:19:51 +0200

    last update

--- app/src/main/java/com/example/airobserver/ui/MainActivity.kt
+++ app/src/main/java/com/example/airobserver/ui/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.airobserver.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.airobserver.R
+import com.google.android.material.button.MaterialButton
 
-    
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
