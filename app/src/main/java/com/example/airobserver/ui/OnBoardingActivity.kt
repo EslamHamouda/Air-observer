15dfbca1e8636b96d9a42d486e13af8a71907f25
eslam <eslam.ee600@gmail.com>
2023-02-15 22:02:08 +0200

    update shared preference

--- app/src/main/java/com/example/airobserver/ui/OnBoardingActivity.kt
+++ app/src/main/java/com/example/airobserver/ui/OnBoardingActivity.kt
@@ -1,35 +1,38 @@
 package com.example.airobserver.ui
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Html
 import android.view.View
-import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.viewpager2.widget.ViewPager2
 import com.example.airobserver.R
 import com.example.airobserver.databinding.ActivityOnBoardingBinding
-import com.example.airobserver.ui.home.ProfileFragment
-import com.example.airobserver.ui.onboarding.SaveState
+import com.example.airobserver.di.SharedPref
 import com.example.airobserver.ui.onboarding.ViewPagerAdapter
+import com.example.airobserver.utils.putData
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class OnBoardingActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityOnBoardingBinding
-    private var saveState: SaveState? = null
     private var images = arrayListOf<Int>()
     private var headings = arrayListOf<Int>()
     private var description = arrayListOf<Int>()
-
+    @Inject
+    lateinit var pref: SharedPreferences
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOnBoardingBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initViewPagerResources()
+
         isFirstRun()
+        initViewPagerResources()
 
         binding.btnNext.setOnClickListener {
             if (getItem(0) < 2) binding.viewPager2.setCurrentItem(
@@ -46,14 +49,14 @@ class OnBoardingActivity : AppCompatActivity() {
         }
 
         binding.btnSkip.setOnClickListener {
-            saveState!!.state = 1
+            pref.putData(SharedPref.FIRST_RUN,true)
             val i = Intent(applicationContext, AuthActivity::class.java)
             startActivity(i)
             finish()
         }
 
         binding.btnGetStarted.setOnClickListener {
-            saveState!!.state = 1
+            pref.putData(SharedPref.FIRST_RUN,true)
             val i = Intent(applicationContext, AuthActivity::class.java)
             startActivity(i)
             finish()
@@ -61,19 +64,9 @@ class OnBoardingActivity : AppCompatActivity() {
 
         binding.viewPager2.adapter = ViewPagerAdapter(this, images, headings, description)
         binding.dotsIndicator.attachTo(binding.viewPager2)
-        binding.viewPager2.registerOnPageChangeCallback(viewListener)
-    }
-
-    private var viewListener: ViewPager2.OnPageChangeCallback =
-        object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageScrolled(
-                position: Int,
-                positionOffset: Float,
-                positionOffsetPixels: Int
-            ) {
-            }
-            @RequiresApi(api = Build.VERSION_CODES.M)
+        binding.viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
                 if (position > 0) {
                     binding.btnPrevious.visibility = View.VISIBLE
                 } else {
@@ -89,8 +82,8 @@ class OnBoardingActivity : AppCompatActivity() {
                     binding.btnSkip.visibility = View.VISIBLE
                 }
             }
-            override fun onPageScrollStateChanged(state: Int) {}
-        }
+        })
+    }
 
     private fun getItem(i: Int): Int {
         return binding.viewPager2.currentItem + i
@@ -115,10 +108,8 @@ class OnBoardingActivity : AppCompatActivity() {
     }
 
     private fun isFirstRun(){
-        saveState = SaveState(this, "onBoarding")
-        if (saveState!!.state == 1) {
-            val i = Intent(this, AuthActivity::class.java)
-            startActivity(i)
+        if (pref.getBoolean(SharedPref.FIRST_RUN,false)){
+            startActivity(Intent(this, AuthActivity::class.java))
             finish()
         }
     }
