862ea9ee5d8831f76d5a3ad8c79331c05fd8e1e7
eslam <eslam.ee600@gmail.com>
2023-03-05 11:48:41 +0200

    update

--- app/src/main/java/com/example/airobserver/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/example/airobserver/ui/auth/RegisterFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -40,6 +43,13 @@ class RegisterFragment : BaseFragment() {
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val adapter=ArrayAdapter(requireContext(),android.R.layout.simple_dropdown_item_1line,
+            arrayOf("Male","Female"))
+        binding.autoCompleteGender.setAdapter(adapter)
+        binding.autoCompleteGender.onItemClickListener=
+            AdapterView.OnItemClickListener { parent, view, position, id -> showSnackbar(parent.getItemAtPosition(position).toString(),requireActivity()) }
+
         binding.tvOrLogin.setOnClickListener {
             findNavController().navigate(RegisterFragmentDirections.actionRegisterFragmentToLoginFragment())
         }
