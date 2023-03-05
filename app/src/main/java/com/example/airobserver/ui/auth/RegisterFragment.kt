cee8fd7f2caede6f7b52ee6b323f9bbede8f4809
eslam <eslam.ee600@gmail.com>
2023-03-05 10:50:00 +0200

    update

--- app/src/main/java/com/example/airobserver/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/example/airobserver/ui/auth/RegisterFragment.kt
@@ -1,18 +1,27 @@
 package com.example.airobserver.ui.auth
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.airobserver.R
 import com.example.airobserver.databinding.ActivityOnBoardingBinding
 import com.example.airobserver.databinding.FragmentRegisterBinding
 import com.example.airobserver.ui.BaseFragment
 import com.example.airobserver.ui.viewmodel.AuthViewModel
+import com.example.airobserver.utils.ApiResponseStates
+import com.example.airobserver.utils.showSnackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class RegisterFragment : BaseFragment() {
@@ -28,10 +37,36 @@ class RegisterFragment : BaseFragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.tvOrLogin.setOnClickListener {
             findNavController().navigate(RegisterFragmentDirections.actionRegisterFragmentToLoginFragment())
+        }
+        binding.btnRegister.setOnClickListener {
+            getRegisterResponse()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun getRegisterResponse() {
+        lifecycleScope.launch {
+            viewModel.registerResponse.flowWithLifecycle(
+                lifecycle,
+                Lifecycle.State.STARTED
+            )
+                .collectLatest {
+                    dataResponseHandling(this@RegisterFragment.requireActivity(),
+                        it,
+                        binding.progressBar.progressBar,
+                        {
+                            viewModel.login(binding.edtEmail.text.toString(),binding.edtPassword.text.toString())
+                        },
+                        { it1 ->
+                            showSnackbar("Registered successful",requireActivity())
+                        })
+                }
+
         }
     }
 
