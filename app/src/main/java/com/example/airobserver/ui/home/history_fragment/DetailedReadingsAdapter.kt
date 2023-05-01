01a1c05f22bf4fc68146e29736b1156d3ccd921e
eslam <eslam.ee600@gmail.com>
2023-05-01 11:46:50 +0300

    update app

--- app/src/main/java/com/example/airobserver/ui/home/history_fragment/DetailedReadingsAdapter.kt
+++ app/src/main/java/com/example/airobserver/ui/home/history_fragment/DetailedReadingsAdapter.kt
@@ -1,7 +1,9 @@
 package com.example.airobserver.ui.home.history_fragment
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.airobserver.databinding.ListItemDetailedReadingsBinding
 import com.example.airobserver.domain.model.response.AqiHistoryDetailsResponse
@@ -14,14 +16,23 @@ class DetailedReadingsAdapter(
     inner class ViewHolder(private val binding: ListItemDetailedReadingsBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(item: AqiHistoryDetailsResponse) {
-            binding.tvPm25.text = item.PM10
-            binding.tvPm10.text = item.PM10
-            binding.tvCo.text = item.CO
-            binding.tvO3.text = item.O3
-            binding.tvSo2.text = item.SO2
-            binding.tvNo2.text = item.NO2
+
             binding.tvTime.text = item.pollutantDate
             binding.tvAqi.text = "AQI: ${item.MAX} | ${item.Category}"
+
+            setDetailedAqi(item.PM10!!.toDouble(),binding.tvPm25) // setTextColor and it depends on the value that is retrieved from database
+
+            setDetailedAqi(item.PM10!!.toDouble(),binding.tvPm10) // setTextColor and it depends on the value that is retrieved from database
+
+            setDetailedAqi(item.CO!!.toDouble(),binding.tvCo) // setTextColor and it depends on the value that is retrieved from database
+
+            setDetailedAqi(item.O3!!.toDouble(),binding.tvO3) // setTextColor and it depends on the value that is retrieved from database
+
+            setDetailedAqi(item.SO2!!.toDouble(),binding.tvSo2) // setTextColor and it depends on the value that is retrieved from database
+
+            setDetailedAqi(item.NO2!!.toDouble(),binding.tvNo2) // setTextColor and it depends on the value that is retrieved from database
+
+            setDetailedAqi(item.MAX!!.toDouble(),binding.tvAqi) // setTextColor and it depends on the value that is retrieved from database
         }
     }
 
@@ -37,6 +48,17 @@ class DetailedReadingsAdapter(
 
     override fun getItemCount(): Int {
         return list.size
+    }
+
+    private fun setDetailedAqi(value: Double, txt: TextView) {
+        when (value) {
+            in 0.0 .. 50.0 -> txt.setTextColor(Color.parseColor("#488A5A"))
+            in 51.0 .. 100.0 -> txt.setTextColor(Color.parseColor("#ddad25"))
+            in 101.0 .. 150.0 -> txt.setTextColor(Color.parseColor("#fc5b00"))
+            in 151.0 .. 200.0 -> txt.setTextColor(Color.parseColor("#c72c2c"))
+            in 201.0 .. 300.0 -> txt.setTextColor(Color.parseColor("#6A359C"))
+            in 301.0 .. 500.0 -> txt.setTextColor(Color.parseColor("#800000"))
+        }
     }
 
 }
