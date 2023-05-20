f19511fcc589dcc5f783a28632c3181420c20659
ahmedasaber <ahmeda.saber22@gmail.com>
2023-05-20 14:05:03 +0300

    edit splash screen

--- app/src/main/java/com/example/airobserver/ui/onboarding/SplashActivity.kt
+++ app/src/main/java/com/example/airobserver/ui/onboarding/SplashActivity.kt
@@ -1,26 +1,31 @@
 package com.example.airobserver.ui.onboarding
 
 import android.content.Intent
-import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
 import com.example.airobserver.R
-import com.example.airobserver.di.SharedPref
-import com.example.airobserver.ui.auth.AuthActivity
+import com.example.airobserver.databinding.ActivityOnBoardingBinding
+import com.example.airobserver.databinding.ActivitySplashBinding
 import javax.inject.Inject
 
-class SplashActivity : AppCompatActivity() {
-    @Inject
 
+class SplashActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val anim = AnimationUtils.loadAnimation(applicationContext, com.example.airobserver.R.anim.splash_anim)
+        binding.gifImageView.startAnimation(anim)
+
+        // the purpose of this Handler is to introduce a delay of 3 seconds before starting the `OnBoardingActivity`
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(applicationContext, OnBoardingActivity::class.java))
             finish()
-        }, 500) // delay time in milliseconds (here 0.5 seconds)
+        }, 3000) // delay time in milliseconds (here 3 seconds)
     }
 }
