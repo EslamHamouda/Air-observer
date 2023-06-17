cd4db21d131833cd6178c3008038d439c280d1ca
eslam <eslam.e600@gmail.com>
2023-06-17 22:59:19 +0300

    commit

--- gaugelibrary/build/generated/source/buildConfig/debug/com/ekndev/gaugelibrary/BuildConfig.java
+++ gaugelibrary/build/generated/source/buildConfig/debug/com/ekndev/gaugelibrary/BuildConfig.java
@@ -4,7 +4,7 @@
 package com.ekndev.gaugelibrary;
 
 public final class BuildConfig {
-  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final boolean DEBUG = false;
   public static final String LIBRARY_PACKAGE_NAME = "com.ekndev.gaugelibrary";
   public static final String BUILD_TYPE = "debug";
 }
