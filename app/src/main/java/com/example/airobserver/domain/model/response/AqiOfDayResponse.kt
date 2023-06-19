1fccc6a972c7032075b2ee340ad4aa4b6a0d4ab3
eslam <eslam.ee600@gmail.com>
2023-06-19 23:40:04 +0300

    update

--- app/src/main/java/com/example/airobserver/domain/model/response/AqiOfDayResponse.kt
+++ app/src/main/java/com/example/airobserver/domain/model/response/AqiOfDayResponse.kt
@@ -3,16 +3,17 @@ package com.example.airobserver.domain.model.response
 import com.google.gson.annotations.SerializedName
 
 data class AqiOfDayResponse(
-    @SerializedName("pollutant_date" ) var pollutantDate : String? = null,
-    @SerializedName("hour"           ) var hour          : String? = null,
-    @SerializedName("PM10"           ) var PM10          : String? = null,
-    @SerializedName("PM25"           ) var PM25          : String? = null,
-    @SerializedName("SO2"            ) var SO2           : String? = null,
-    @SerializedName("CO"             ) var CO            : String? = null,
-    @SerializedName("O3"             ) var O3            : String? = null,
-    @SerializedName("NO2"            ) var NO2           : String? = null,
-    @SerializedName("MAX"            ) var MAX           : String? = null,
-    @SerializedName("Critical"       ) var Critical      : String? = null,
-    @SerializedName("Category"       ) var Category      : String? = null
-
+    @SerializedName("id"            ) var id            : String? = null,
+    @SerializedName("Date"          ) var Date          : String? = null,
+    @SerializedName("Time"          ) var Time          : String? = null,
+    @SerializedName("NO2"           ) var NO2           : String? = null,
+    @SerializedName("CO"            ) var CO            : String? = null,
+    @SerializedName("O3"            ) var O3            : String? = null,
+    @SerializedName("PM10"          ) var PM10          : String? = null,
+    @SerializedName("PM25"          ) var PM25          : String? = null,
+    @SerializedName("SO2"           ) var SO2           : String? = null,
+    @SerializedName("Max"           ) var Max           : String? = null,
+    @SerializedName("CriticalValue" ) var CriticalValue : String? = null,
+    @SerializedName("Category"      ) var Category      : String? = null,
+    @SerializedName("Note"          ) var Note          : String? = null
 )
